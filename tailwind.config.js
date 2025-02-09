633b91569cf9567312a66b82336c7a3265a14ee1
Shaurya Upadhyay <shaurya01836@gmail.com>
2025-02-09 14:40:08 +0530

    initialize frontend folder

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+
+      },
+    },
+  },
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Include all your file paths
+  plugins: [],
+};
