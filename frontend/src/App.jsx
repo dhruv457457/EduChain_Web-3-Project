608dcb545e13b521cd1e225ce5d3cd85dc9ab4cb
Shaurya Upadhyay <shaurya01836@gmail.com>
2025-02-09 23:38:49 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+
 import Transfer from "./pages/Transfer";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/transfer" element={<Transfer />} />
