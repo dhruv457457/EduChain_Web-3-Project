08c55b018ae7e9d36803003a160c6c8c03c55395
Shaurya Upadhyay <shaurya01836@gmail.com>
2025-02-09 15:45:18 +0530

    initialize routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Transfer from "./pages/Transfer";
+import Home from "./pages/Home";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Transfer />} />
+        <Route path="/contact" element={<Transfer />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <>
+     <nav className="bg-gray-800 p-4 text-white flex justify-around">
+        <Link to="/" className="hover:text-blue-300">
+          Home
+        </Link>
+        <Link to="/about" className="hover:text-green-300">
+          About
+        </Link>
+        <Link to="/contact" className="hover:text-yellow-300">
+          Contact
+        </Link>
+      </nav>
+   
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
