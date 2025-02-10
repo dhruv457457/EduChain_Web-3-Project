1c9f1af573c069ad40aad3c926faf830e635d12d
Nitin Jain <nitinjain18072005@gmail.com>
2025-02-11 00:17:02 +0530

    change_cards

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -3,15 +3,16 @@ import { FaShieldAlt, FaUsers, FaPiggyBank } from 'react-icons/fa';
 
 function Cards({ title, description, icon }) {
   return (
-    <div className="box w-full sm:w-96 h-64 bg-slate-950 border-2 border-purple-500 rounded-xl shadow-custom-purple flex flex-col justify-center items-center p-6 
-      transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:shadow-purple-500/50 cursor-pointer">
+    <div className="group box w-full sm:w-96 h-64 bg-slate-950 border-2 border-purple-500 rounded-xl shadow-custom-purple flex flex-col justify-center items-center p-6 
+      transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:shadow-purple-500/50 cursor-pointer ">
       <div className="flex items-center space-x-3">
         {icon}
-        <h1 className="text-white text-xl font-bold">{title}</h1>
+        <h1 className="text-white text-xl font-bold group-hover:text-customPurple">{title}</h1>
       </div>
       <p className="text-white mt-3 text-md px-3">{description}</p>
+      
     </div>
   );
 }
-
+ 
 export default Cards;
