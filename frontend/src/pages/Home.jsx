bafe9bc1702b82109d56848abef1c79631f17f9f
Nitin Jain <nitinjain18072005@gmail.com>
2025-02-10 18:55:08 +0530

    ..changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
+import Cards from "../components/Cards";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Header />
+      <Cards />
     </>
   );
 }
