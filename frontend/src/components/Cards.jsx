75076e09f8d97128ab4a7c737b99617bc703f9e1
Shaurya Upadhyay <shaurya01836@gmail.com>
2025-02-11 19:33:59 +0530

    Update Cards.jsx

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { FaShieldAlt, FaUsers, FaPiggyBank } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 
 function Cards({ title, description, icon }) {
   return (
-    <div className="group box w-full sm:w-96 h-64 bg-slate-950 border-2 border-purple-500 rounded-xl shadow-custom-purple flex flex-col justify-center items-center p-6 
-      transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:shadow-purple-500/50 cursor-pointer ">
+    <motion.div
+      whileHover={{ scale: 1.05, boxShadow: "0px 10px 20px rgba(128, 0, 128, 0.3)" }}
+      transition={{ type: "spring", stiffness: 200, damping: 10 }}
+      className="w-full sm:w-96 h-64 bg-slate-950 border-2 border-customPurple rounded-xl shadow-custom-purple flex flex-col justify-center items-center p-6 cursor-pointer"
+    >
       <div className="flex items-center space-x-3">
         {icon}
-        <h1 className="text-white text-xl font-bold group-hover:text-customPurple">{title}</h1>
+        <h1 className="text-white text-xl font-bold hover:text-customPurple">{title}</h1>
       </div>
       <p className="text-white mt-3 text-md px-3">{description}</p>
-      
-    </div>
+    </motion.div>
   );
 }
- 
+
 export default Cards;
