8e40ca0ca726de70f9642ee56c098270a91a7afa
Shaurya Upadhyay <shaurya01836@gmail.com>
2025-02-11 19:34:02 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,11 +7,11 @@ export default function Footer() {
     <hr />
     <div className="relative bg-customSemiPurple h-auto w-full py-7 px-4">
        <div className='flex flex-col md:flex-row py-7 px-4 md:px-14 gap-10 md:gap-96'>
-        <div className='text-center md:text-left'>
-            <h1 className='text-4xl font-bold bg-gradient-to-r from-blue-500 via-purple-600 to-purple-900 text-transparent bg-clip-text'>
-                FortressPay
+        <div className='text-center md:text-left '>
+            <h1 className='text-4xl font-bold text-customBlue text-shadow-custom '>
+                Cryptify
                 </h1>
-            <p className='py-6 text-xl bg-gradient-to-r from-teal-700 via-olive-600 to-yellow-300 text-transparent bg-clip-text'>
+            <p className='py-6 text-lg text-white opacity-80 text-shadow-custom'>
             Where Every Payment is Fortified—Shop with Peace of Mind!
             </p>
         </div>
@@ -21,14 +21,14 @@ export default function Footer() {
             Company
             </h1>
             <div className='text-lg text-white mt-2 px-3'>
-            <ul className="text-lg mt-2">
-            <li><a href="#" className="hover:underline">About us</a></li>
-            <li><a href="#" className="hover:underline">Careers</a></li>
+            <ul className="text-lg mt-2 opacity-80">
+            <li><a href="#" className="hover:text-gray-300">About us</a></li>
+            <li><a href="#" className="hover:text-gray-300">Careers</a></li>
           </ul>
            </div>
            </div>
            <div>
-            <h1 className='text-2xl text-white font-bold'>Contact</h1>
+            <h1 className='text-2xl text-white font-bold '>Contact</h1>
             <div className="flex justify-center md:justify-start space-x-3 mt-3">
             <a href="#" className="text-white text-lg hover:text-gray-400"><FaFacebook /></a>
             <a href="#" className="text-white text-lg hover:text-gray-400"><FaTwitter /></a>
@@ -37,8 +37,8 @@ export default function Footer() {
            </div>
         </div>
        </div>
-       <div className="border-t border-gray-600 mt-2 pt-6 text-center text-xl text-white">
-       &copy; 2025 FortressPay, Inc. All rights reserved. | Empowering the future of digital transactions.
+       <div className="border-t border-customPurple mt-2 pt-6 text-center text-lg text-white opacity-80 text-shadow-custom ">
+       &copy; 2025 Cryptify, Inc. All rights reserved. | Empowering the future of digital transactions.
        </div>
       </div>
       
