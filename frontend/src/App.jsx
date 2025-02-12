48f900e23033d2cab728acdaec336803c5c06b05
Dhruv Pancholi <dpancholi.pp123@gmail.com>
2025-02-12 16:48:55 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Transfer from "./pages/Transfer";
 import Home from "./pages/Home";
-
+import User from "./pages/User";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/transfer" element={<Transfer />} />
+        <Route path="/user" element={<User />} />
+
       </Routes>
     </Router>
   );
