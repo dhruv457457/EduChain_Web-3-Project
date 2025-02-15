db6d03c4809aeccb7b40384b4c47500dfca5cec5
Dhruv Pancholi <dpancholi.pp123@gmail.com>
2025-02-15 19:20:57 +0530

    Update UserDashboard.jsx

--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import UserBalance from "./UserBalance";
 import RegisterName from "./RegisterName";
+import UserTransactions from "./UserTransactions";
 
 const UserDashboard = () => {
   return (
     <div className="flex flex-col lg:flex-row items-center bg-customSemiPurple py-20 px-4 justify-evenly h-screen">
       <UserBalance />
       <RegisterName />
+      <UserTransactions />
     </div>
   );
 };
